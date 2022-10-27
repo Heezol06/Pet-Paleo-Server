22c0ca1c4f70a7b44c2103aadf6c5980b3c2f407
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-10-27 21:44:07 +0600

    delete food

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const dotenv = require("dotenv");
+const ObjectId = require('mongodb').ObjectId;
 
 // use middleware
 app.use(cors());
@@ -28,7 +29,6 @@ async function run() {
     await client.connect();
     const foodsCollection = client.db("PetDB").collection("Pet-Foods");
 
-
     // add a food
     app.post("/addFood", async (req, res) => {
       const newFood = req.body;
@@ -36,17 +36,17 @@ async function run() {
       const result = await foodsCollection.insertOne(newFood);
       res.send(result);
     });
-// get all foods 
-app.get("/allFood", async (req, res)=>{
-  const query = {}
-  const cursor = foodsCollection.find(query)
-  const food = await cursor.toArray();
-  res.send(food)
-})
-
-
-
-
+    // get all foods
+    app.get("/allFood", async (req, res) => {
+      const query = {};
+      const cursor = foodsCollection.find(query);
+      const food = await cursor.toArray();
+      res.send(food);
+    });
+    //delete a food item
+    app.delete("/allFood/:id", async(req, res) => {
+      const id = req.params.id;
+    })
   } finally {
     // await client.close();
   }
